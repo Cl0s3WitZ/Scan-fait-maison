b7e8ab33838f5e9118d28fce24813cd777e7f400
Cl0s3WitZ <128795072+Cl0s3WitZ@users.noreply.github.com>
2025-03-24 18:39:01 +0100

    Update ScanFaitMaison.py
    Changement de certains mots

--- ScanFaitMaison.py
+++ ScanFaitMaison.py
@@ -5,46 +5,46 @@ import random
 
 #==========FONCTIONS==============
 def randomPorts(pMin, pMax, num_ranges):
-	range_list = [(i, i + num_ranges - 1) for i in range(pMin, pMax + 1, num_ranges)]
-	random.shuffle(range_list)
-	return range_list
+    range_list = [(i, i + num_ranges - 1) for i in range(pMin, pMax + 1, num_ranges)]
+    random.shuffle(range_list)
+    return range_list
 
 #===========SCAN TCP=============
-def LeScan(pMin, pMax, num_ranges, LesFlags, TypeScan, ipCible):
-	ListePorts = randomPorts(pMin, pMax, num_ranges)
-	print ("\nScan en cours...")
-	listPortsOuverts = []
-	listPortsFermer = []
+def leScan(pMin, pMax, num_ranges, lesFlags, typeScan, ipCible):
+    listePorts = randomPorts(pMin, pMax, num_ranges)
+    print("\nScan en cours...")
+    listePortsOuverts = []
+    listePortsFermes = []
 
-#--------------Envoi des packets----------------
-	for i in ListePorts:
-		trame = Ether()/IP(dst=ipCible)/TCP(sport=57915, dport=i, flags=LesFlags)
-		reponse = srp(trame, timeout=1, verbose=False)
+    #--------------Envoi des paquets----------------
+    for i in listePorts:
+        trame = Ether()/IP(dst=ipCible)/TCP(sport=57915, dport=i, flags=lesFlags)
+        reponse = srp(trame, timeout=1, verbose=False)
 
-#-------------------------Récuperation des reponses----------------------------
-		for y in range(len(reponse[0])):
-			reponseFlags = (reponse[0][y][1].getlayer(TCP).flags)
-			reponsePort = (reponse[0][y][1].getlayer(TCP).sport)
-			if reponseFlags != "RA":
-				listPortsOuverts.append((reponsePort, reponseFlags)
-			if reponseFlags == "RA" or reponseFlags == "R":
-				listPortsFermer.append((reponsePort))
+        #-------------------------Récupération des réponses----------------------------
+        for y in range(len(reponse[0])):
+            reponseFlags = (reponse[0][y][1].getlayer(TCP).flags)
+            reponsePort = (reponse[0][y][1].getlayer(TCP).sport)
+            if reponseFlags != "RA":
+                listePortsOuverts.append((reponsePort, reponseFlags))
+            if reponseFlags == "RA" or reponseFlags == "R":
+                listePortsFermes.append(reponsePort)
 
-#---------------------------Analise des reponses--------------------------------
-	print ("\nLa liste des ports ouverts : ")
-	for r1 in listPortsOuverts:
-		print ("port =",r1[0]," ---------->reponse =",r1[1])
-	if listPortsFermer == []:
-		print ("Machine non vunerable au scan")
-	else:
-		print ("\nLa liste des ports potentiellement ouverts : ")
-		for r2 in range(pMin, pMax):
-			if r2 not in listPortsFermer:
-				print ("port = ",r2)
+    #---------------------------Analyse des réponses--------------------------------
+    print("\nLa liste des ports ouverts : ")
+    for r1 in listePortsOuverts:
+        print("port =", r1[0], " ----------> réponse =", r1[1])
+    if listePortsFermes == []:
+        print("Machine non vulnérable au scan")
+    else:
+        print("\nLa liste des ports potentiellement ouverts : ")
+        for r2 in range(pMin, pMax):
+            if r2 not in listePortsFermes:
+                print("port =", r2)
 
 #=============SCAN UDP=======================
 
-#Pas encore developper
+#ToDo
 
 #=#=#=#=#=#=#=#=#=#=LANCEMENT DU SCAN=#=#=#=#=#=#=#=#=#=#=#=#
 
@@ -54,35 +54,36 @@ def LeScan(pMin, pMax, num_ranges, LesFlags, TypeScan, ipCible):
 print("IP Cible :")
 ipCible = input()
 print("\nPort minimum :")
-pMin = input()
+pMin = int(input())
 print("\nPort maximum :")
-pMax = input()
+pMax = int(input())
 
 #========SCAN UDP==========
 
-#Pas encore developper
+#ToDo
 
 #========SCAN TCP SIMPLE===========
-print ("\n\nSCAN TCP SIMPLE (SYN)")
-print ("###############")
-LeScan(pMin, pMax, 20, "S", "SIMPLE", ipCible)
+print("\n\nSCAN TCP SIMPLE (SYN)")
+print("#####################")
+leScan(pMin, pMax, 20, "S", "SIMPLE", ipCible)
 
 #===========SCAN FIN==============
-print ("\nSCAN FIN")
-print ("########")
-LeScan(pMin, pMax, 20, "F", "OTHER", ipCible)
+print("\nSCAN FIN")
+print("########")
+leScan(pMin, pMax, 20, "F", "OTHER", ipCible)
 
 #=======SCAN Maimon (FIN/ACK)=====
-print ("SCAN Maimon")
-print ("###########")
-LeScan(pMin, pMax, 20, "FA", "OTHER", ipCible)
+print("SCAN Maimon")
+print("###########")
+leScan(pMin, pMax, 20, "FA", "OTHER", ipCible)
 
 #==========SCAN NULL==============
-print ("\nSCAN NULL")
-print ("#########")
-LeScan(pMin, pMax, 20, "", "OTHER", ipCible)
+print("\nSCAN NULL")
+print("#########")
+leScan(pMin, pMax, 20, "", "OTHER", ipCible)
 
 #==========SCAN Xmas==============
-print ("\nSCAN Xmas")
-print ("########")
-LeScan(pMin, pMax, 20, "FPU", "OTHER", ipCible)
+print("\nSCAN Xmas")
+print("########")
+leScan(pMin, pMax, 20, "FPU", "OTHER", ipCible)
+
